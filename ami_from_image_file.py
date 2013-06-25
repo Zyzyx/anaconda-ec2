4df97a31c03d42c086569bab887b9e602e148eaa
Jay Greguske <jgregusk@redhat.com>
2013-06-25 19:49:08 -0400

    clean up ami_from_image_file.py

--- ami_from_image_file.py
+++ ami_from_image_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #   Copyright (C) 2013 Red Hat, Inc.
 #   Copyright (C) 2013 Ian McLeod <imcleod@redhat.com>
+#                      Jay Greguske <jgregusk@redhat.com>
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -14,29 +15,29 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import logging
-import sys
+from optparse import OptionParser
+import os.path
 from aws_utils import EBSHelper, AMIHelper
 
-if len(sys.argv) != 5:
-    print
-    print "Create an AMI on EC2 from a bootable image file"
-    print
-    print "usage: %s <ec2_region> <ec2_key> <ec2_secret> <image_file>" % sys.argv[0]
-    print
-    sys.exit(1)
+def get_opts():
+    usage = """%prog [options] ec2_key ec2_secret image_file
 
-region = sys.argv[1]
-key = sys.argv[2]
-secret = sys.argv[3]
-image_file = sys.argv[4]
+Create an AMI on EC2 from a bootable disk image."""
+    parser = OptionParser(usage=usage)
+    parser.add_option('-r', '--region', default='us-east-1',
+        help='set an EC2 region (us-east-1)')
+    opts.args = parser.parse_args()
+    if len(args) != 3:
+        parser.error('You must provide an EC2 Access Key, Secret, and image')
+    if not os.path.exists(args[2]):
+        parser.error('Could not find %s' % args[2])
+    return opts.region, args[0], args[1], args[2]
 
-logging.basicConfig(level=logging.DEBUG, format='%(message)s')
-
-ebs_helper = EBSHelper(region, key, secret)
-snapshot = ebs_helper.safe_upload_and_shutdown(image_file)
-
-ami_helper = AMIHelper(region, key, secret)
-ami = ami_helper.register_ebs_ami(snapshot)
+if __name__ == '__main__':
+    region, key, secret, image_file = get_opts()
+    ebs_helper = EBSHelper(region, key, secret)
+    snapshot = ebs_helper.safe_upload_and_shutdown(image_file)
+    ami_helper = AMIHelper(region, key, secret)
+    ami = ami_helper.register_ebs_ami(snapshot)
 
 print "Got AMI: %s" % ami
